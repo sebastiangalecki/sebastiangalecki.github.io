6c91408d9b361d6556b9ad5dbb9f5d27f7260c82
kucujemny <51801824+kucujemny@users.noreply.github.com>
2020-01-24 22:33:57 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var tc = 'nadają orgonity (super bonus kurwo xD)';
+﻿var tc = 'nadają orgonity (super bonus kurwo xD)';
 
 var t = [
 'Globaliści',
@@ -112,9 +112,21 @@ function initTable() {
     });
 }
 
+Date.prototype.getWeek = function() {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                        - 3 + (week1.getDay() + 6) % 7) / 7);
+}
+
 function randomizeTable() {
     let d = new Date();
-    let seed = d.getDate() + ((d.getMonth() + 1) * 100) + (d.getFullYear() * 10000);
+    let seed = d.getWeek() + ((d.getMonth() + 1) * 100) + (d.getFullYear() * 10000);
     t = szufla(t, seed);
 
     for (let i = 0; i < tw*tw; i++) {
