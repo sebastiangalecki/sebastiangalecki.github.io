5d66975ef616931756b3fba4ff62d8669b0d39d5
malkovich <malkovich@users.noreply.github.com>
2021-10-23 02:01:09 +0200

    Zapis stanu krzyżyków na bingo

--- js/script.js
+++ js/script.js
@@ -111,6 +111,7 @@ function initTable() {
         else {
             $(this).addClass('crossed');
         }
+        saveBingoState();
     });
 }
 
@@ -202,6 +203,31 @@ function resetTable() {
     fillTable();
 }
 
+function saveBingoState() {
+    let krzyze = new Array();
+    for (td of document.querySelectorAll('table#table td')) {
+        if (td.className.includes("crossed")) { 
+            krzyze.push(td.attributes['data-cell'].value)
+        }
+    }
+    Cookies.set('krzyze', JSON.stringify(krzyze), { expires: 999 });
+}
+
+function restoreBingoState() {
+    if (Cookies.get('krzyze')!==undefined) {
+        let krzyze = JSON.parse(Cookies.get('krzyze'));
+        for (k of krzyze) {
+            cell = document.querySelector(`table#table td[data-cell="${k}"]`)
+            if (!cell.className.includes("crossed")) {
+                cell.classList.add("crossed");
+            }
+            console.log(k);
+        }
+    }
+}
+
+
+
 $(document).ready(function() {
     var loaded = Cookies.get('values')
     
@@ -223,4 +249,5 @@ $(document).ready(function() {
     initTable();
     randomizeTable();
     fillTable();
+    restoreBingoState();
 });
