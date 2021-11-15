6846c7b6069476f5fe3e7993a9ea9f8df4b880a8
kucujemny <51801824+kucujemny@users.noreply.github.com>
2021-11-15 15:27:20 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -260,7 +260,7 @@ function saveBingoState() {
     }
     var sunday = new Date()
     sunday.setDate(sunday.getDate() + 6 - (sunday.getDay() + 6) % 7);
-    sunday.setHours(23,59,59)
+    sunday.setHours(23,59)
     console.log(sunday)
     Cookies.set('krzyze', JSON.stringify(krzyze), { expires: sunday }) //nie ma po co trzymać to ciasteczko w nieskończoność
 }
